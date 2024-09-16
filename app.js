3a62a05814f7fdd9cc23e89778d703f8d23e7b05
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2024-09-16 07:45:13 +0100

    fixed the dynamic display of the books

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const myLibrary = [];
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pageNumber = document.getElementById('number-of-pages');
+const submitBtn = document.querySelector('.btn');
+
+const myLibrary = [
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    numberOfPages: 200,
+    read: true
+  }
+];
 
 function Book(title, author, numberOfPages, read) {
   this.title = title;
@@ -10,3 +22,48 @@ Book.prototype.info = () => {
   return `${this.title} by ${this.author} has ${this.numberOfPages} pages, not read yet`;
 }
 
+
+function addBookToLibrary() {
+  if(!title.value || !author.value || pageNumber.value) {
+    alert('Please fill in all the fields.');
+    return;
+  }
+  const isRead = document.querySelector('input[type="radio"]:checked')?.value;
+
+  // create a new instance of the Book constructor
+  const book = new Book(title.value, author.value, pageNumber.value, isRead === 'false');
+
+  myLibrary.push(book);
+  displayBooks(myLibrary);
+  clearInput();
+}
+
+function displayBooks(data) {
+  const tableBody = document.getElementById('table-body');
+  tableBody.innerHTML = '';
+
+  data.forEach(item => {
+    const tableRow = document.createElement('tr');
+
+    Object.values(item).forEach(value => {
+      const tableData = document.createElement('td');
+      tableData.textContent = value;
+
+      tableRow.appendChild(tableData);
+    })
+
+    tableBody.appendChild(tableRow);
+  })
+
+
+}
+
+function clearInput() {
+  title.value = '';
+  author.value = '';
+  pageNumber.value = '';
+  document.querySelectorAll('input[type="radio"]').forEach(radio => radio.checked = false);
+
+}
+
+submitBtn.addEventListener('click', addBookToLibrary);
